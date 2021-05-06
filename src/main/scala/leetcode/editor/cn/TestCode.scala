081f3eed0ccefd7bdd8ecc83253d3c5477fa0548
Mengkai <mengkai@xiaomi.com>
2021-05-06 20:24:36 +0800

    feat(P1):

---
+++ src/main/scala/leetcode/editor/cn/TestCode.scala
@@ -0,0 +1,16 @@
+package leetcode.editor.cn
+
+/**
+ * @author MengKai
+ * @create 2021-05-06
+ * @mail mengkai@xiaomi.com
+ */
+object TestCode {
+    def main(args: Array[String]): Unit = {
+        val array = Array(1, 2, 3, 4)
+        for ((k, v) <- array.zipWithIndex) {
+            println(k,v)
+        }
+
+    }
+}
